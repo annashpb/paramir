8dfa61dcd85fdba3fca9d34404061d9d099793f6
Anna Sh <anna.sh.pb@gmail.com>
2020-03-25 20:51:42 +0200

    added book card styles

--- front-page.php
+++ front-page.php
@@ -12,24 +12,24 @@
                 <?php while ($the_query->have_posts()) : $the_query->the_post(); ?>
                     <li class="books-preview__item">
                         <a href="<?php the_permalink(); ?>" class="books-preview__link">
-                            <img src="<?php echo get_field('cover_photo')['url']; ?>" alt="<?php echo get_field('cover_photo')['alt']; ?>" width="310px" height="425px" class="books-preview__cover">
-                            <div class="books-preview__descr--short">
+                            <img src="<?php echo get_field('cover_photo')['url']; ?>" alt="<?php echo get_field('cover_photo')['alt']; ?>" class="books-preview__cover">
+                            <div class="books-preview__descr books-preview__descr--short">
                                 <p class="books-preview__author"><?php the_field('author'); ?></p>
                                 <h3 class="books-preview__name"><?php the_field('name'); ?></h2>
                             </div>
-                            <div class="books-preview__descr--full">
+                            <div class="books-preview__descr books-preview__descr--full">
                                 <p class="books-preview__author"><?php the_field('author'); ?></p>
                                 <h3 class="books-preview__name"><?php the_field('name'); ?></h2>
-                                    <div class="books-preview__additional">
-                                        <?php
-                                        $isAvailable = get_field('is_available');
-                                        if ($isAvailable) : ?>
+                                    <?php
+                                    $isAvailable = get_field('is_available');
+                                    if ($isAvailable) : ?>
+                                        <div class="books-preview__additional">
                                             <p class="books-preview__price"><?php the_field('price'); ?></p>
                                             <button class="default-btn books-preview__basket" type="button"><?php _e('В корзину'); ?></button>
-                                        <?php else : ?>
-                                            <p class="books-preview__available--not"><?php _e('Нет в наличии'); ?></p>
-                                        <?php endif; ?>
-                                    </div>
+                                        </div>
+                                    <?php else : ?>
+                                        <p class="books-preview__available--not"><?php _e('Нет в наличии'); ?></p>
+                                    <?php endif; ?>
                             </div>
                         </a>
                     </li>
