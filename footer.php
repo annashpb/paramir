37b1ed0e7aba4b400ca33a6bbb8587a937bfe75b
Anna Sh <anna.sh.pb@gmail.com>
2020-03-27 21:01:13 +0200

    modified footer styles

--- footer.php
+++ footer.php
@@ -43,16 +43,14 @@
 		</div>
 		<div class="site-footer__block">
 			<h3 class="site-footer__headline"><?php _e('Личный кабинет'); ?></h3>
-			<a href="#" class="site-footer__link"><?php _e('Вход'); ?></a>
-			<a href="#" class="site-footer__link"><?php _e('Регистрация'); ?></a>
+			<p class="site-footer__text"><a href="#" class="site-footer__link"><?php _e('Вход'); ?></a></p>
+			<p class="site-footer__text"><a href="#" class="site-footer__link"><?php _e('Регистрация'); ?></a></p>
 		</div>
 		<div class="site-footer__block">
 			<h3 class="site-footer__headline"><?php _e('Мы в социальных сетях'); ?></h3>
 			<?php if (have_rows('social_links', 'option')) : ?>
 				<?php while (have_rows('social_links', 'option')) : the_row(); ?>
-					<a href="<?php echo get_sub_field('social_link_address')['url']; ?>" class="site-footer__socials" target="<?php echo get_sub_field('social_link_address')['target']; ?>" title="<?php echo get_sub_field('social_link_address')['title']; ?>" style="background: url('<?php the_sub_field('social_link_icon'); ?>') center no-repeat;">
-						<span><?php echo get_sub_field('social_link_address')['title']; ?></span>
-					</a>
+					<a href="<?php echo get_sub_field('social_link_address')['url']; ?>" class="site-footer__socials" target="<?php echo get_sub_field('social_link_address')['target']; ?>" title="<?php echo get_sub_field('social_link_address')['title']; ?>" style="background: url('<?php the_sub_field('social_link_icon'); ?>') center no-repeat;"></a>
 				<?php endwhile; ?>
 			<?php endif; ?>
 		</div>
